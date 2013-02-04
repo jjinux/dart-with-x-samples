cd03039c524c9bf518f90a2635e7dc7814533cd5
Shannon -jj Behrens <jjinux@gmail.com>
2013-02-04 14:14:43 -0800

    The GWT code can now say that it is loaded

--- gwt/GwtApplication/src/org/dartlang/gwtapplication/client/GwtApplication.java
+++ gwt/GwtApplication/src/org/dartlang/gwtapplication/client/GwtApplication.java
@@ -1,6 +1,9 @@
 package org.dartlang.gwtapplication.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -11,5 +14,11 @@ public class GwtApplication implements EntryPoint {
    * This is the entry point method.
    */
   public void onModuleLoad() {
+    VerticalPanel mainPanel = new VerticalPanel();
+    RootPanel.get("gwtDiv").add(mainPanel);
+    
+    Label gwtLoadedLabel = new Label();
+    gwtLoadedLabel.setText("GWT application loaded");
+    mainPanel.add(gwtLoadedLabel);
   }
 }
