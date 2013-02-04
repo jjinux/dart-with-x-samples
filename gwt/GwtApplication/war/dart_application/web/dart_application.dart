94d1f8e5249b8e86efc8bf1b7b60c6fa3bc1587f
Shannon -jj Behrens <jjinux@gmail.com>
2013-02-04 15:41:58 -0800

    Dart can now parse the JSON coming from the server

--- gwt/GwtApplication/war/dart_application/web/dart_application.dart
+++ gwt/GwtApplication/war/dart_application/web/dart_application.dart
@@ -1,6 +1,19 @@
 import "dart:html";
+import "dart:json" as JSON;
 
 main() {
+  // Say that the application has been loaded.
   var div = query("#dartDiv");
   div.appendHtml("<div>Dart application loaded</div>");
+  
+  // Request some JSON from the server. Of course, parsing it here is
+  // overkill.
+  var url = "/gwt_application/json_servlet";
+  new HttpRequest.get(url, (req) {
+    var json = JSON.parse(req.responseText);
+    div.appendHtml("""
+      <div>Here's the JSON I got: <span id="jsonData"></span></div>
+    """);
+    query("#jsonData").text = JSON.stringify(json);
+  });
 }
