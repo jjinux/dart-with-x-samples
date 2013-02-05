682c5de2898c354d7110afcf1dfb1bc2ff498963
Shannon -jj Behrens <jjinux@gmail.com>
2013-02-04 16:19:14 -0800

    Handle postMessage in Dart

--- gwt/GwtApplication/war/dart_application/web/dart_application.dart
+++ gwt/GwtApplication/war/dart_application/web/dart_application.dart
@@ -7,13 +7,34 @@ main() {
   div.appendHtml("<div>Dart application loaded</div>");
   
   // Request some JSON from the server. Of course, parsing it here is
-  // overkill.
+  // silly, but I'm showing it for completeness.
   var url = "/gwt_application/json_servlet";
   new HttpRequest.get(url, (req) {
     var json = JSON.parse(req.responseText);
-    div.appendHtml("""
-      <div>Here's the JSON I got: <span id="jsonData"></span></div>
-    """);
-    query("#jsonData").text = JSON.stringify(json);
+    var escaped = htmlEscape(JSON.stringify(json));
+    div.appendHtml("<div>Here's the JSON I got: $escaped</div>");
   });
+
+  // Add a button to call postMessage.
+  var postMessageButton = new ButtonElement()
+    ..id = "postMessage"
+    ..text = "Post message from Dart"
+    ..classes.add("gwt-Button")  // For consistency
+    ..onClick.listen((e) => window.postMessage("Hello from Dart!", "*"));
+  div.children.add(postMessageButton);
+  
+  // Receive postMessage events.
+  window.onMessage.listen((e) {
+    div.appendHtml("<div>Dart received postMessage: "
+                   "Data: ${htmlEscape(e.data)} "
+                   "Origin: ${htmlEscape(e.origin)}</div>");
+  });
+}
+        
+String htmlEscape(String text) {
+  return text.replaceAll("&", "&amp;")
+      .replaceAll("<", "&lt;")
+      .replaceAll(">", "&gt;")
+      .replaceAll('"', "&quot;")
+      .replaceAll("'", "&apos;");
 }
